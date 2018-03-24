658674de25c49b3c98b835fd5592a4028aafb23c
Greg <rabasse.greg@gmail.com>
2018-03-24 21:26:12 +0100

    deploy heroku

--- server/config.production.js
+++ server/config.production.js
@@ -1,24 +1,6 @@
 'use strict';
 
 module.exports = {
-  'restApiRoot': '/api',
   'host': 'https://les-aiguilleurs.herokuapp.com/',
   'port': process.env.STRONGLOOP_PORT || 80,
-  'remoting': {
-    'context': false,
-    'rest': {
-      'handleErrors': false,
-      'normalizeHttpPath': false,
-      'xml': false,
-    },
-    'json': {
-      'strict': false,
-      'limit': '100kb',
-    },
-    'urlencoded': {
-      'extended': true,
-      'limit': '100kb',
-    },
-    'cors': false,
-  },
 };
