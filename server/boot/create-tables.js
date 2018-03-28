a613f4939fd219139c586cccec463ae756bcd1af
Greg <rabasse.greg@gmail.com>
2018-03-28 20:24:42 +0200

    deploy

--- server/boot/create-tables.js
+++ server/boot/create-tables.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var server = require('./server');
+var server = require('../server');
 var ds = server.dataSources.heroku || server.dataSources.db;
 var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Artiste', 'Utilisateur'];
 ds.autoupdate(lbTables, function(er) {
