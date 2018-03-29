12586c60c6062554224513e57b17336727e17eff
Greg <rabasse.greg@gmail.com>
2018-03-29 18:45:09 +0200

    deploy

--- server/create-tables.js
+++ server/create-tables.js
@@ -3,7 +3,7 @@
 var server = require('./server');
 var ds = server.dataSources.heroku || server.dataSources.db;
 var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Artiste', 'Utilisateur'];
-ds.automigrate(lbTables, function(er) {
+ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
   ds.disconnect();
 });
