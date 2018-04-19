2cbf91edffb1cbf1ba65d954a319075455205870
Greg <rabasse.greg@gmail.com>
2018-04-19 15:32:21 +0200

    retour à la bdd de heroku

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,13 +2,15 @@
 
 module.exports = {
   'db': {
-    'host': 'world-321.fr.planethoster.net',
-    'port': 3306,
-    'database': 'lesaepqx_aiguilleurs',
-    'password': 'KPXnQHWpx6FUfwh1b2',
+    'host': 'ec2-79-125-12-27.eu-west-1.compute.amazonaws.com',
+    'port': 5432,
+    'url': process.env.DATABASE_URL || 'postgres://nhznbxakvkregu:7d7e6c426eeff9bd1e8df70e4b8caca8c78667851cd95a030e4d0efc5b8bf078@ec2-79-125-12-27.eu-west-1.compute.amazonaws.com:5432/d1vl9aag43cck7',
+    'ssl': true,
+    'database': 'd1vl9aag43cck7',
+    'password': '7d7e6c426eeff9bd1e8df70e4b8caca8c78667851cd95a030e4d0efc5b8bf078',
     'name': 'db',
-    'user': 'lesaepqx_user',
-    'connector': 'mysql',
+    'user': 'nhznbxakvkregu',
+    'connector': 'postgresql',
   },
   'local-storage': {
     'name': 'local-storage',
